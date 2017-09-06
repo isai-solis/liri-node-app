1da100820233520ed03e6bda6d45e7235ddc7b5a
isai-solis <solis_isai@yahoo.com>
2017-09-05 19:26:41 -0500

    Set up repository, file structure, api key information listed in keys file.

---
+++ app.js
@@ -0,0 +1,3 @@
+var spotify = require("");
+var twitter = require("");
+var request = require("");
---
+++ keys.js
@@ -0,0 +1,20 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+// Consumer Key (API Key)	AvOdcVhLN3dsQc7DOUVd3aZyc
+// Consumer Secret (API Secret)	IbogMkYPlzzAIzq1cIWResVLhnfwoGiVv9WBBRCdgNETVpTjjr
+// Access Level	Read and write (modify app permissions)
+// Owner	solisisai1
+// Owner ID	903753557626822658
+
+// Access Token	903753557626822658-IEkn6dDgHTERduzaJS6bDDXlhD0mR6X
+// Access Token Secret	yPDJ35Par9uu2W9nkO7NRzfp5mD9pMUNtzc6VhmoVdHbP
+// Access Level	Read and write
+// Owner	solisisai1
+// Owner ID	903753557626822658
